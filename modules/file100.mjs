b003b80c6aee64fa9c81323a77f1eea00890ecaf
Alexander Myshov <simplex.light@gmail.com>
2018-10-02 23:51:35 +0700

    Fix timers

--- modules/file100.mjs
+++ modules/file100.mjs
@@ -1,2 +1,7 @@
 const el = document.getElementById('file100');
 el.innerText = ' ...';
+
+const currentTime = new Date();
+const elapsed = currentTime - window.h1startTime;
+const h1time = document.getElementById('h1time');
+h1time.innerText = elapsed + ' ms';
--- modules/h2file100.mjs
+++ modules/h2file100.mjs
@@ -1,2 +1,7 @@
 const el = document.getElementById('h2file100');
 el.innerText = ' ...';
+
+const currentTime = new Date();
+const elapsed = currentTime - window.h2startTime;
+const h2time = document.getElementById('h2time');
+h2time.innerText = elapsed + ' ms';
