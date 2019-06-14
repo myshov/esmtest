52cf4cf193d0264d9552127fb40197cfdf4e2f84
Alexander Myshov <simplex.light@gmail.com>
2019-06-15 01:01:50 +0700

    Fix styles and layout

--- modules/file100.mjs
+++ modules/file100.mjs
@@ -4,4 +4,4 @@ el.innerText = ' ...';
 const currentTime = new Date();
 const elapsed = currentTime - window.h1startTime;
 const h1time = document.getElementById('h1time');
-h1time.innerText = elapsed + ' ms';
+h1time.innerText = `Loaded in ${elapsed} ms`;
--- modules/h2file100.mjs
+++ modules/h2file100.mjs
@@ -4,4 +4,4 @@ el.innerText = ' ...';
 const currentTime = new Date();
 const elapsed = currentTime - window.h2startTime;
 const h2time = document.getElementById('h2time');
-h2time.innerText = elapsed + ' ms';
+h2time.innerText = `Loaded in ${elapsed} ms`;
